d61d8ed507dcf3cd35e5403c79ea4effe19559a9
KV-Systems <development@kv-sys.ru>
2019-02-21 16:49:37 +0500

    added observer

---
+++ System/Service/Monitor/Command/OnCommand.php
@@ -0,0 +1,59 @@
+<?php
+namespace Evie\Monitor\System\Service\Monitor\Command;
+
+use Evie\Monitor\System\Request\Request;
+use Evie\Monitor\System\Service\Monitor\Background\BackgroundProcess;
+
+/**
+ * Class OnCommand.
+ * Rejects the execution of the system command.
+ * @package Evie\MonitorController\System\Command
+ */
+class OnCommand implements ICommand {
+
+    /**
+     * Processes identifiers list.
+     * @var $_list array
+     */
+    private $_list = [];
+
+    /**
+     * Application request.
+     * @var $_request Request
+     */
+    private $_request;
+
+    /**
+     * Sets request.
+     * StartCommand constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)   {
+        $this->_request = $request;
+    }
+
+    /**
+     * Executes default command.
+     * @return bool
+     */
+    public function execute(): bool {
+        $servers = $this->_request->parameter('list')->value();
+        $servers = explode(',', $servers);
+        if(is_array($servers) && !empty($servers)) {
+            foreach($servers as $ipa) {
+                $process = new BackgroundProcess();
+                $process->run('php ' . ROOT_DIR . 'index.php -m monitor -r monitor/poll -a ' . $ipa );
+                if($pid = $process->getPid()) $this->_list[] = $pid;
+            }
+        }
+        return count($this->_list) == count($servers) ? true : false;
+    }
+
+    /**
+     * Gets process identifier.
+     * @return int
+     */
+    public function pid(): int  {
+        return 0;
+    }
+}
