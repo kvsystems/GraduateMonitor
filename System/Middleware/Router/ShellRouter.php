0e55c04332b969cd47fd83c0bf95083f474b39ac
KV-Systems <development@kv-sys.ru>
2019-02-21 10:16:49 +0500

    changed shell routing

--- System/Middleware/Router/ShellRouter.php
+++ System/Middleware/Router/ShellRouter.php
@@ -1,9 +1,11 @@
 <?php
 namespace Evie\Monitor\System\Middleware\Router;
 
+use Evie\Monitor\System\Config;
 use Evie\Monitor\System\Controller\Responder;
 use Evie\Monitor\System\Middleware\Base\Middleware;
 use Evie\Monitor\System\Request\Request;
+use Evie\Monitor\System\Response\IResponse;
 
 /**
  * Class ShellRouter.
@@ -25,35 +27,60 @@ class ShellRouter implements IRouter  {
     private $_middleware = [];
 
     /**
-     * Handlers list.
-     * @var $_handlers array
+     * Routes list.
+     * @var $_routes array
      */
-    private $_handlers   = [];
+    private $_routes = [];
 
+    /**
+     * Sets responder and available routes.
+     * ShellRouter constructor.
+     * @param Responder $responder
+     * @param array $routes
+     */
     public function __construct(Responder $responder, array $routes){
         $this->_responder = $responder;
+        $config = new Config();
+        $this->_routes = $config->getRoutes();
     }
 
-    private function numbers(Request $request) : array {
-        $path = explode('/', trim($request->path(), '/'));
-        //array_unshift($path, $method);
-        return $this->_routes->match($path);
+    /**
+     * Gets request route.
+     * @param Request $request
+     * @return array
+     */
+    private function _match(Request $request) : array {
+        $path = str_replace('/', '@', $request->path());
+        return in_array($path, $this->_routes)
+            ? explode('@', $path, '@')
+            : explode('@', $this->_routes[0], '@');
     }
 
-    public function handle(Request $request)   {
-        $routeNumbers = $this->_getRouteNumbers($request);
-        if (count($routeNumbers) == 0) {
-            return $this->_responder->error();
-        }
-        return call_user_func($this->_routeHandlers[$routeNumbers[0]], $request);
+    /**
+     * Handles request.
+     * @param Request $request
+     * @return IResponse
+     */
+    public function handle(Request $request) : IResponse {
+        $controller = $this->_match($request);
+        return call_user_func($controller, $request);
     }
 
+    /**
+     * Loads middleware.
+     * @param Middleware $middleware
+     */
     public function load(Middleware $middleware)    {
         $next = count($this->_middleware) > 0 ? $this->_middleware[0] : $this;
         $middleware->setNext($next);
         array_unshift($this->_middleware, $middleware);
     }
 
+    /**
+     * Handles middleware.
+     * @param Request $request
+     * @return IResponse
+     */
     public function route(Request $request)    {
         return count($this->_middleware) > 0
             ? $this->_middleware[0]->handle($request)
