af5fe7f513455c1a3360261dc9007b3b21d33471
KV-Systems <development@kv-sys.ru>
2019-02-21 14:57:23 +0500

    server key added (-s)

--- System/Request/RequestFactory.php
+++ System/Request/RequestFactory.php
@@ -21,7 +21,7 @@ class RequestFactory {
                 $request = new HttpRequest();
                 break;
             case self::CLI:
-                $request = new ShellRequest(["m:r:"]);
+                $request = new ShellRequest(["m:r:s:"]);
                 break;
             default:
                 $request = new DefaultRequest();
