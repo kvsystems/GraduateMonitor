f0faa027b6fce76d9e0e18bcc189d36c635d4c65
KV-Systems <development@kv-sys.ru>
2019-02-21 10:23:39 +0500

    changed return of FW:handle

--- System/Middleware/FirewallMiddleware.php
+++ System/Middleware/FirewallMiddleware.php
@@ -3,10 +3,17 @@ namespace Evie\Monitor\System\Middleware;
 
 use Evie\Monitor\System\Middleware\Base\Middleware;
 use Evie\Monitor\System\Request\Request;
+use Evie\Monitor\System\Response\DefaultResponse;
+use Evie\Monitor\System\Response\IResponse;
 
 class FirewallMiddleware extends Middleware {
 
-    public function handle(Request $request)   {
-        echo 'FIREWALL_ENABLED' . PHP_EOL;
+    /**
+     * Handles firewall middleware.
+     * @param Request $request
+     * @return IResponse
+     */
+    public function handle(Request $request) : IResponse  {
+        return new DefaultResponse();
     }
 }
