81bb3c46a83eb44d76d2fc238748ce52aee7424a
KV-Systems <development@kv-sys.ru>
2019-02-23 22:16:11 +0500

    commented

--- index.php
+++ index.php
@@ -3,10 +3,16 @@ namespace Evie\Monitor;
 
 use Evie\Monitor\System\Core;
 
+/** Manual autoload **/
 define('ROOT_DIR', realpath( dirname(__FILE__) ) . DIRECTORY_SEPARATOR);
 require_once ROOT_DIR . 'System/Autoload.php';
 
-Core::instance()
-    ->init()
-    ->handle()
-    ->output();
+/** Trying main script execution **/
+try {
+    Core::instance()
+        ->init()
+        ->handle()
+        ->output();
+} catch (\Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
